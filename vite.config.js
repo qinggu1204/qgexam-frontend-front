ff36b57d91625c75867e13dc845fbf732632ae1e
josephGu <josephgu0323@outlook.com>
2023-01-02 20:18:19 +0800

    初步实现动态锚点

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,14 @@ export default defineConfig({
         },
     },
     server: {
-        port: 5173, // 端口  
+        port: 5173, // 端口 
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080/',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     },
 
 })

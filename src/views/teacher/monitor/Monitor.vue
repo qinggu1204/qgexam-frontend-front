abcf6c0c4577abbb11913bc5e0aa7379912d210c
josephGu <josephgu0323@outlook.com>
2023-01-21 19:21:17 +0800

    修复bug

--- src/views/teacher/monitor/Monitor.vue
+++ src/views/teacher/monitor/Monitor.vue
@@ -60,7 +60,21 @@
     <a-col :span="6">
       <a-card style="width: 350px;" hoverable>
         <div class="video">
-          <video loop muted autoplay src="./videos/1.mp4" style="height: 200px">
+          <video loop muted autoplay src="./videos/2.mp4" style="height: 200px">
+            您的浏览器不支持视频播放
+          </video>
+        </div>
+        <a-card-meta title="操作系统">
+          <template #description>
+            学生 202003151106
+          </template>
+        </a-card-meta>
+      </a-card>
+    </a-col>
+    <a-col :span="6">
+      <a-card style="width: 350px;" hoverable>
+        <div class="video">
+          <video loop muted autoplay src="./videos/3.mp4" style="height: 200px">
             您的浏览器不支持视频播放
           </video>
         </div>
@@ -88,21 +102,7 @@
     <a-col :span="6">
       <a-card style="width: 350px;" hoverable>
         <div class="video">
-          <video loop muted autoplay src="./videos/1.mp4" style="height: 200px">
-            您的浏览器不支持视频播放
-          </video>
-        </div>
-        <a-card-meta title="操作系统">
-          <template #description>
-            学生 202003151106
-          </template>
-        </a-card-meta>
-      </a-card>
-    </a-col>
-    <a-col :span="6">
-      <a-card style="width: 350px;" hoverable>
-        <div class="video">
-          <video loop muted autoplay src="./videos/1.mp4" style="height: 200px">
+          <video loop muted autoplay src="./videos/2.mp4" style="height: 200px">
             您的浏览器不支持视频播放
           </video>
         </div>
